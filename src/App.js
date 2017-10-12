ab825c8e1fa33bac0b794e22b2e09c4370a6da36
Ratchagarn Naewbuntad <ratchagarn@omise.co>
2017-10-12 19:31:44 +0700

    Add currency to db.

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ export default connect((state) => state)(
           <Card key={i}>
             <p>{item.name}</p>
             {payments}
-            <button onClick={handlePay.call(self, item.id, self.state.selectedAmount)}>Pay</button>
+            <button onClick={handlePay.call(self, item.id, self.state.selectedAmount, item.currency)}>Pay</button>
           </Card>
         );
       });
@@ -84,12 +84,12 @@ export default connect((state) => state)(
   }
 );
 
-function handlePay(id, amount) {
+function handlePay(id, amount, currency) {
   const self = this;
   return function() {
     fetch('http://localhost:3001/payments', {
       method: 'POST',
-      body: `{ "charitiesId": ${id}, "amount": ${amount} }`,
+      body: `{ "charitiesId": ${id}, "amount": ${amount}, "currency": "${currency}" }`,
     })
       .then(function(resp) { return resp.json(); })
       .then(function() {
