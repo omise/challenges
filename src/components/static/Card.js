fac1d002f32632e8f4b0524e8d433a18ff19d439
Beta Anggoro <ben@benworkandplay.com>
2017-12-18 03:48:33 +0700

    Refactored to static component - Card

---
+++ src/components/static/Card.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    display: inline-block;
+    vertical-align: top;
+    width: 29%;
+    position: relative;
+    margin: 10px;
+    padding: 18px;
+    border: 1px solid #ccc;
+`;
