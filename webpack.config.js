d2c20c805aca0414a3125d6f1e9d2eea7da47a6b
Beta Anggoro <ben@benworkandplay.com>
2017-12-18 03:35:40 +0700

    Updated webpack to use html-webpack-plugin to inject js and other html elements like title & favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,15 @@
-const path = require('path');
+import paths from './paths';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
 
 const config = {
   entry: './src/index.js',
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: 'build',
+    filename: 'js/[name].js',
+    path: paths.appBuild,
+    publicPath: '/',
   },
 
   devtool: 'inline-source-map',
-
-  devServer: {
-    inline: true,
-    host: '0.0.0.0',
-    port: 3000,
-    historyApiFallback: true,
-    disableHostCheck: true,
-    contentBase: 'public',
-  },
 
   module: {
     rules: [
@@ -28,7 +20,39 @@ const config = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 40000,
+              name: 'images/[hash]-[name].[ext]',
+            },
+          },
+          'image-webpack-loader',
+        ] 
+      }
     ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      filename: 'index.html',
+      template: paths.appIndexHtmlTemplate,
+      title: 'Omise Tamboon React',
+      favicon: `${paths.appPublic}/favicon.ico`,
+      inject: true,
+    }),
+  ],
+
+  devServer: {
+    inline: true,
+    host: '0.0.0.0',
+    port: 3000,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    contentBase: 'public',
   },
 };
 
