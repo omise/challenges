ce7e2034052e5cff189e8b386b589d56eeef486d
Beta Anggoro <ben@benworkandplay.com>
2018-01-03 13:36:37 +0700

    Components refactor - Static components to be more reusable

--- src/components/static/Button.js
+++ src/components/static/Button.js
@@ -1,25 +1,30 @@
 import styled from 'styled-components';
-import {palette} from 'config/cssVars';
+import {palette} from 'config/variables';
 
 export default styled.button`
   background: transparent;
   color: ${palette.blueDark};
-  border-color: ${palette.blue};
+  border-color: ${palette.blueDark};
   transition: background 0.2s ease-in-out;
   transform: translateY(0);
 
-  &:focus {
+  &:active {
     background: transparent;
     color: ${palette.blueDark};
   }
 
-  &:hover {
+  &:hover,
+  &:focus {
     background: ${palette.blueDark};
     color: white;
   }
 
   &:active {
     transform: translateY(2px);
+  }
+
+  &:disabled {
+    opacity: 0.4;
   }
 
 `;
