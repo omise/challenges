42df48bbae71e99c402bbfe1b1c7303fe97d409f
doston9471 <doston.7194@gmail.com>
2020-02-25 03:09:40 +0600

    fix race condition

--- app/models/charity.rb
+++ app/models/charity.rb
@@ -2,6 +2,8 @@ class Charity < ActiveRecord::Base
   validates :name, presence: true
 
   def credit_amount(amount)
-    update_column :total, total + amount
+    self.with_lock do
+      update_column :total, total + amount
+    end
   end
 end
